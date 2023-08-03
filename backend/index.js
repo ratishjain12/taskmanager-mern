bc30dd2c8ed89496e94e32b400e0703259e8c3e7
ratishjain <ratishjain6@gmail.com>
2023-08-03 20:58:52 +0530

    task manager done

--- backend/index.js
+++ backend/index.js
@@ -8,59 +8,54 @@ const app = express();
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
-mongoose
-  .connect(
-    `mongodb+srv://ratishjain:${process.env.MONGO_PASSWORD}@cluster0.ibsssgs.mongodb.net/?retryWrites=true&w=majority`
-  )
-  .then(() => {
-    app.get("/", (req, res) => {
-      res.json("Testing");
-    });
+mongoose.connect(
+  `mongodb+srv://ratishjain:${process.env.MONGO_PASSWORD}@cluster0.ibsssgs.mongodb.net/?retryWrites=true&w=majority`
+);
 
-    app.get("/fetchtasks", async (req, res) => {
-      const tasks = await Task.find({});
-      res.json(tasks);
-    });
+app.get("/", (req, res) => {
+  res.json("Testing");
+});
 
-    app.post("/addtask", async (req, res) => {
-      const { id, title, description } = req.body;
-      const post = await Task.create({ id, title, description });
-      res.json("Successfully added task");
-    });
+app.get("/fetchtasks", async (req, res) => {
+  const tasks = await Task.find({});
+  res.json(tasks);
+});
 
-    app.post("/done/:id", async (req, res) => {
-      const id = req.params.id;
-      console.log(id);
-      await Task.deleteOne({ id: id });
-      res.json("Task deleted successfully");
-    });
+app.post("/addtask", async (req, res) => {
+  const { id, title, description } = req.body;
+  const post = await Task.create({ id, title, description });
+  res.json("Successfully added task");
+});
 
-    app.post("/updatetask/:id", async (req, res) => {
-      const id = req.params.id;
-      const { status } = req.body;
-      console.log(status);
-      await Task.updateOne({ id: id }, { status: status });
-      res.json("done");
-    });
+app.post("/done/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  await Task.deleteOne({ id: id });
+  res.json("Task deleted successfully");
+});
 
-    app.post("/update/:id", async (req, res) => {
-      const id = req.params.id;
-      const { title, description } = req.body;
-      await Task.updateOne(
-        { id: id },
-        { title: title, description: description }
-      );
-      res.json("done");
-    });
+app.post("/updatetask/:id", async (req, res) => {
+  const id = req.params.id;
+  const { status } = req.body;
+  console.log(status);
+  await Task.updateOne({ id: id }, { status: status });
+  res.json("done");
+});
 
-    app.post("/updatetask/:id", async (req, res) => {
-      const id = req.params.id;
-      const { status } = req.body;
-      console.log(status);
-      await Task.updateOne({ id: id }, { status: status });
-      res.json("done");
-    });
-  });
+app.post("/update/:id", async (req, res) => {
+  const id = req.params.id;
+  const { title, description } = req.body;
+  await Task.updateOne({ id: id }, { title: title, description: description });
+  res.json("done");
+});
+
+app.post("/updatetask/:id", async (req, res) => {
+  const id = req.params.id;
+  const { status } = req.body;
+  console.log(status);
+  await Task.updateOne({ id: id }, { status: status });
+  res.json("done");
+});
 
 app.listen(3000, () => {
   console.log("server started");
